655e94139fc8fe6eaac2ef08752803dcd2971519
Vikrant Malla <vikrantmalla999@gmail.com>
2024-12-27 22:47:58 +0545

    feat: init setup

---
+++ server/eslint.config.mjs
@@ -0,0 +1,12 @@
+import pluginJs from "@eslint/js";
+import eslintPrettierConfig from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPrettierConfig,
+];
